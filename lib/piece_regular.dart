33c021027caa096ad41db73432ce96504850bc1d
knightingal <yujianming1985@hotmail.com>
2024-02-10 13:10:21 +0800

    add parseXiang

--- lib/piece_regular.dart
+++ lib/piece_regular.dart
@@ -76,3 +76,41 @@ List<PosInfo> parseShuai(PieceInfo pieceInfo, ChessPlayGround chessPlayGround) {
 
   return posList;
 }
+
+List<PosInfo> parseXiang(PieceInfo pieceInfo, ChessPlayGround chessPlayGround) {
+  int maxX, maxY, minX, minY;
+  if (pieceInfo.player == 1) {
+    minX = 0;
+    minY = 0;
+    maxX = 8;
+    maxY = 4;
+  } else {
+    minX = 0;
+    minY = 5;
+    maxX = 8;
+    maxY = 9;
+  }
+  List<PosInfo> posList = [
+    PosInfo(pieceInfo.x + 2, pieceInfo.y + 2),
+    PosInfo(pieceInfo.x - 2, pieceInfo.y - 2),
+    PosInfo(pieceInfo.x + 2, pieceInfo.y - 2),
+    PosInfo(pieceInfo.x - 2, pieceInfo.y + 2),
+  ];
+
+  posList.removeWhere((e) {
+    if (e.x < minX || e.x > maxX) {
+      return true;
+    }
+    if (e.y < minY || e.y > maxY) {
+      return true;
+    }
+    var target = chessPlayGround.findTarget(e.x, e.y);
+    if (target.player == pieceInfo.player) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return posList;
+}

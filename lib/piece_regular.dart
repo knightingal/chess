0d2f0133100e347891b07b7a5c93701785f9d458
knightingal <yujianming1985@hotmail.com>
2024-02-10 13:05:35 +0800

    add parseShi

--- lib/piece_regular.dart
+++ lib/piece_regular.dart
@@ -1,5 +1,44 @@
 import 'playground.dart';
 
+List<PosInfo> parseShi(PieceInfo pieceInfo, ChessPlayGround chessPlayGround) {
+  int maxX, maxY, minX, minY;
+  if (pieceInfo.player == 1) {
+    minX = 3;
+    minY = 0;
+    maxX = 5;
+    maxY = 2;
+  } else {
+    minX = 3;
+    minY = 7;
+    maxX = 5;
+    maxY = 9;
+  }
+
+  List<PosInfo> posList = [
+    PosInfo(pieceInfo.x + 1, pieceInfo.y + 1),
+    PosInfo(pieceInfo.x - 1, pieceInfo.y - 1),
+    PosInfo(pieceInfo.x + 1, pieceInfo.y - 1),
+    PosInfo(pieceInfo.x - 1, pieceInfo.y + 1),
+  ];
+
+  posList.removeWhere((e) {
+    if (e.x < minX || e.x > maxX) {
+      return true;
+    }
+    if (e.y < minY || e.y > maxY) {
+      return true;
+    }
+    var target = chessPlayGround.findTarget(e.x, e.y);
+    if (target.player == pieceInfo.player) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return posList;
+}
+
 List<PosInfo> parseShuai(PieceInfo pieceInfo, ChessPlayGround chessPlayGround) {
   int maxX, maxY, minX, minY;
   if (pieceInfo.player == 1) {

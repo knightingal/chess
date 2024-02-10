0e9460d05b7c05e448bdd6be7f1d5157c8115439
knightingal <yujianming1985@hotmail.com>
2024-02-10 14:41:39 +0800

    add parseMa

--- lib/piece_regular.dart
+++ lib/piece_regular.dart
@@ -126,3 +126,56 @@ List<PosInfo> parseXiang(PieceInfo pieceInfo, ChessPlayGround chessPlayGround) {
 
   return posList;
 }
+
+List<PosInfo> parseMa(PieceInfo pieceInfo, ChessPlayGround chessPlayGround) {
+  List<PosInfo> calPathPos(PosInfo start, PosInfo end) {
+    var diffY = end.y - start.y;
+    var diffX = end.x - start.x;
+    if (diffY == -2 || diffY == 2) {
+      return [PosInfo(start.x, start.y + diffY ~/ 2)];
+    } else {
+      return [PosInfo(start.x + diffX ~/ 2, start.y)];
+    }
+  }
+
+  int maxX, maxY, minX, minY;
+  minX = 0;
+  minY = 0;
+  maxX = 8;
+  maxY = 9;
+  List<PosInfo> posList = [
+    PosInfo(pieceInfo.x + 2, pieceInfo.y + 1),
+    PosInfo(pieceInfo.x + 2, pieceInfo.y - 1),
+    PosInfo(pieceInfo.x + 1, pieceInfo.y + 2),
+    PosInfo(pieceInfo.x + 1, pieceInfo.y - 2),
+    PosInfo(pieceInfo.x - 2, pieceInfo.y + 1),
+    PosInfo(pieceInfo.x - 2, pieceInfo.y - 1),
+    PosInfo(pieceInfo.x - 1, pieceInfo.y + 2),
+    PosInfo(pieceInfo.x - 1, pieceInfo.y - 2),
+  ];
+
+  posList.removeWhere((e) {
+    if (e.x < minX || e.x > maxX) {
+      return true;
+    }
+    if (e.y < minY || e.y > maxY) {
+      return true;
+    }
+    var target = chessPlayGround.findTarget(e.x, e.y);
+    if (target.player == pieceInfo.player) {
+      return true;
+    }
+
+    var pathPos = calPathPos(PosInfo(pieceInfo.x, pieceInfo.y), e);
+    if (pathPos.any((e) {
+      var target = chessPlayGround.findTarget(e.x, e.y);
+      return target.valid();
+    })) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return posList;
+}

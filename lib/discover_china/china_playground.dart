9be3857877d71d5083fda6d52673c036eebf03f1
knightingal <yujianming1985@hotmail.com>
2024-03-10 22:08:09 +0800

    add player list

--- lib/discover_china/china_playground.dart
+++ lib/discover_china/china_playground.dart
@@ -29,13 +29,69 @@ class CityNameWidget extends StatelessWidget {
   final int x;
   final int y;
   final String name;
+  final List<int> playerList;
 
   const CityNameWidget(
       {super.key,
       required this.gridSize,
       required this.x,
       required this.y,
-      required this.name});
+      required this.name,
+      required this.playerList});
+
+  List<Widget> getPlayerWidgetList() {
+    List<Widget> playerWidgetList = [];
+    if (playerList.contains(0)) {
+      playerWidgetList.add(
+        Positioned(
+            left: 0,
+            top: 0,
+            width: gridSize / 2,
+            height: gridSize / 2,
+            child: Container(
+              color: Colors.red,
+            )),
+      );
+    }
+    if (playerList.contains(1)) {
+      playerWidgetList.add(
+        Positioned(
+            left: gridSize / 2,
+            top: 0,
+            width: gridSize / 2,
+            height: gridSize / 2,
+            child: Container(
+              color: Colors.blue,
+            )),
+      );
+    }
+    if (playerList.contains(2)) {
+      playerWidgetList.add(
+        Positioned(
+            left: 0,
+            top: gridSize / 2,
+            width: gridSize / 2,
+            height: gridSize / 2,
+            child: Container(
+              color: Colors.orange,
+            )),
+      );
+    }
+    if (playerList.contains(3)) {
+      playerWidgetList.add(
+        Positioned(
+            left: gridSize / 2,
+            top: gridSize / 2,
+            width: gridSize / 2,
+            height: gridSize / 2,
+            child: Container(
+              color: Colors.green,
+            )),
+      );
+    }
+
+    return playerWidgetList;
+  }
 
   @override
   Widget build(BuildContext context) => Positioned(
@@ -43,20 +99,27 @@ class CityNameWidget extends StatelessWidget {
       top: gridSize * y,
       width: gridSize,
       height: gridSize,
-      child: Container(
-          color: Colors.amber,
-          child: FittedBox(
-            fit: BoxFit.none,
-            child: Text(
-              name,
-              overflow: TextOverflow.visible,
-              maxLines: 1,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: gridSize / 3,
-              ),
-            ),
-          )));
+      child: Stack(
+        children: [
+          ...getPlayerWidgetList(),
+          SizedBox(
+              width: gridSize,
+              height: gridSize,
+              // color: Colors.amber,
+              child: FittedBox(
+                fit: BoxFit.none,
+                child: Text(
+                  name,
+                  overflow: TextOverflow.visible,
+                  maxLines: 1,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: gridSize / 3,
+                  ),
+                ),
+              ))
+        ],
+      ));
 }
 
 class DiscoverPlayGroundWidget extends StatelessWidget {
@@ -76,15 +139,63 @@ class DiscoverPlayGroundWidget extends StatelessWidget {
                       gridSize * widthGridCount, gridSize * heightGridCount),
                   painter: PlayGroundCustomPainter(gridSize: gridSize),
                 ),
-                CityNameWidget(gridSize: gridSize, x: 6, y: 5, name: "北京"),
-                CityNameWidget(gridSize: gridSize, x: 5, y: 5, name: "天津"),
-                CityNameWidget(gridSize: gridSize, x: 4, y: 7, name: "呼和浩特"),
-                CityNameWidget(gridSize: gridSize, x: 3, y: 10, name: "西双版纳"),
-                CityNameWidget(gridSize: gridSize, x: 4, y: 10, name: "昆明"),
-                CityNameWidget(gridSize: gridSize, x: 10, y: 5, name: "南京"),
-                CityNameWidget(gridSize: gridSize, x: 11, y: 6, name: "上海"),
-                CityNameWidget(gridSize: gridSize, x: 11, y: 7, name: "杭州"),
-                CityNameWidget(gridSize: gridSize, x: 12, y: 7, name: "台北"),
+                CityNameWidget(
+                  gridSize: gridSize,
+                  x: 6,
+                  y: 4,
+                  name: "北京",
+                  playerList: [0, 2, 1, 3],
+                ),
+                CityNameWidget(
+                  gridSize: gridSize,
+                  x: 5,
+                  y: 5,
+                  name: "天津",
+                  playerList: [],
+                ),
+                CityNameWidget(
+                  gridSize: gridSize,
+                  x: 4,
+                  y: 7,
+                  name: "呼和浩特",
+                  playerList: [],
+                ),
+                CityNameWidget(
+                    gridSize: gridSize,
+                    x: 3,
+                    y: 10,
+                    name: "西双版纳",
+                    playerList: []),
+                CityNameWidget(
+                    gridSize: gridSize,
+                    x: 4,
+                    y: 10,
+                    name: "昆明",
+                    playerList: []),
+                CityNameWidget(
+                    gridSize: gridSize,
+                    x: 10,
+                    y: 5,
+                    name: "南京",
+                    playerList: []),
+                CityNameWidget(
+                    gridSize: gridSize,
+                    x: 11,
+                    y: 6,
+                    name: "上海",
+                    playerList: []),
+                CityNameWidget(
+                    gridSize: gridSize,
+                    x: 11,
+                    y: 7,
+                    name: "杭州",
+                    playerList: []),
+                CityNameWidget(
+                    gridSize: gridSize,
+                    x: 12,
+                    y: 7,
+                    name: "台北",
+                    playerList: []),
               ],
             )));
   }
@@ -111,7 +222,8 @@ class PlayGroundCustomPainter extends CustomPainter {
       canvas.drawLine(
           calOffset(i, 0), calOffset(i, heightGridCount - 1), paint);
     }
-    canvas.drawLine(calOffset(6, 5), calOffset(11, 6), paint);
+    canvas.drawLine(calOffset(6, 4), calOffset(11, 6), paint);
+    canvas.drawLine(calOffset(10, 5), calOffset(11, 6), paint);
   }
 
   Offset calOffset(int x, int y) {

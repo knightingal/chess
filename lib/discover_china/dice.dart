33f4a97681dd2bf24890008ef3721438b24cb4ab
knightingal <yujianming1985@hotmail.com>
2024-03-20 21:54:18 +0800

    generate random number as dice

--- lib/discover_china/dice.dart
+++ lib/discover_china/dice.dart
@@ -1,3 +1,6 @@
+import 'dart:developer' as dev;
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DiceWidget extends StatelessWidget {
@@ -11,11 +14,20 @@ class DiceWidget extends StatelessWidget {
       width: gridSize * 10,
       height: gridSize * 5,
       color: Colors.lightBlue,
-      child: Row(
-        children: [
-          SingleDice(gridSize: gridSize, color: Colors.red),
-          SingleDice(gridSize: gridSize, color: Colors.yellow),
-        ],
+      child: GestureDetector(
+        onTapDown: (e) {
+          // dev.log("dice taped");
+          int dice1 = Random().nextInt(6) + 1;
+          int dice2 = Random().nextInt(6) + 1;
+
+          dev.log("dice taped $dice1 $dice2");
+        },
+        child: Row(
+          children: [
+            SingleDice(gridSize: gridSize, color: Colors.red),
+            SingleDice(gridSize: gridSize, color: Colors.yellow),
+          ],
+        ),
       ),
     );
   }

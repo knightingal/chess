f606117039e1bbb785630beccea46c2b1c7fbd69
knightingal <yujianming1985@hotmail.com>
2024-03-22 20:38:59 +0800

    make dice stateful

--- lib/discover_china/dice.dart
+++ lib/discover_china/dice.dart
@@ -3,35 +3,59 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class DiceWidget extends StatelessWidget {
+class DiceWidget extends StatefulWidget {
   final double gridSize;
 
   const DiceWidget({super.key, required this.gridSize});
 
   @override
+  State<DiceWidget> createState() => DiceState();
+}
+
+class DiceState extends State<DiceWidget> {
+  DiceState();
+
+  int dice1 = Random().nextInt(6);
+  int dice2 = Random().nextInt(6);
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      width: gridSize * 10,
-      height: gridSize * 5,
+      width: widget.gridSize * 10,
+      height: widget.gridSize * 5,
       color: Colors.lightBlue,
       child: GestureDetector(
         onTapDown: (e) {
           // dev.log("dice taped");
-          int dice1 = Random().nextInt(6) + 1;
-          int dice2 = Random().nextInt(6) + 1;
+          dice1 = Random().nextInt(6);
+          dice2 = Random().nextInt(6);
 
           dev.log("dice taped $dice1 $dice2");
+          setState(() {});
         },
         child: Row(
           children: [
-            SingleDice(gridSize: gridSize, color: Colors.red),
-            SingleDice(gridSize: gridSize, color: Colors.yellow),
+            SingleDice(gridSize: widget.gridSize, color: diceColors[dice1]),
+            SingleDice(gridSize: widget.gridSize, color: diceColors[dice2]),
           ],
         ),
       ),
     );
   }
 }
+
+List<Color> diceColors = [
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.orange,
+  Colors.purple,
+  Colors.white
+];
 
 class SingleDice extends StatelessWidget {
   final double gridSize;

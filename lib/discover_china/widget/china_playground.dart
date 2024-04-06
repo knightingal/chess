44a4dc6b012d7be0748e2b9b160625494fc16379
knightingal <yujianming1985@hotmail.com>
2024-04-06 21:25:41 +0800

    split out ticket

--- lib/discover_china/widget/china_playground.dart
+++ lib/discover_china/widget/china_playground.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 
 import 'city_card.dart';
 import 'dice.dart';
+import 'ticket.dart';
 import 'widget_static_data.dart';
 
 class DiscoverApp extends StatelessWidget {
@@ -309,85 +310,6 @@ class PlayConsolePad extends StatelessWidget {
             // city list
           ],
         ),
-      ),
-    );
-  }
-}
-
-class TicketList extends StatefulWidget {
-  final double gridSize;
-
-  const TicketList({super.key, required this.gridSize});
-
-  @override
-  State<StatefulWidget> createState() {
-    return TicketState();
-  }
-}
-
-class TicketState extends State<TicketList> {
-  @override
-  Widget build(BuildContext context) {
-    var gridSize = widget.gridSize;
-    return Container(
-      width: gridSize * 10,
-      height: gridSize * 3,
-      color: Colors.green,
-      child: Consumer<GameModel>(builder: (context, game, child) {
-        var ticketCount = game.playerDataList[game.getTabPlayer()].ticketCount;
-        return Row(
-          children: [
-            SignleTicket(
-              gridSize: gridSize,
-              color: Colors.red,
-              count: ticketCount[0],
-            ),
-            SignleTicket(
-              gridSize: gridSize,
-              color: Colors.purple,
-              count: ticketCount[1],
-            ),
-            SignleTicket(
-              gridSize: gridSize,
-              color: Colors.blue,
-              count: ticketCount[2],
-            ),
-            SignleTicket(
-              gridSize: gridSize,
-              color: Colors.green,
-              count: ticketCount[3],
-            ),
-            SignleTicket(
-              gridSize: gridSize,
-              color: Colors.orange,
-              count: ticketCount[4],
-            ),
-          ],
-        );
-      }),
-    );
-  }
-}
-
-class SignleTicket extends StatelessWidget {
-  final double gridSize;
-  final Color color;
-  final int count;
-
-  const SignleTicket(
-      {super.key,
-      required this.gridSize,
-      required this.color,
-      required this.count});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: gridSize * 2,
-      height: gridSize * 3,
-      color: color,
-      child: Center(
-        child: Text(count.toString()),
       ),
     );
   }

882511274ec5e8b3079d2f34b299979856c2af3c
knightingal <yujianming1985@hotmail.com>
2024-04-06 21:15:22 +0800

    move to widget dir

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 import 'chess/chess_app.dart';
-import 'discover_china/china_playground.dart'
+import 'discover_china/widget/china_playground.dart'
     hide heightGridCount, widthGridCount;
 
 void main() {

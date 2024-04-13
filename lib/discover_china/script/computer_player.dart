8c04b4e75458a4f20fe4f021087457123e183efa
knightingal <yujianming1985@hotmail.com>
2024-04-13 23:09:51 +0800

    finish dijkstra

--- lib/discover_china/script/computer_player.dart
+++ lib/discover_china/script/computer_player.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class FindPath {}
 
 class DJNode {
@@ -18,8 +20,44 @@ class Path {
   late DJNode targetNode;
   List<DJNode> path = [];
   int distance = 0;
+
+  Path({required this.targetNode, required this.path, required this.distance});
 }
 
 List<Path> dijkstra(List<DJNode> graph, DJNode vNode) {
-  return [];
+  List<Path> sList = [Path(targetNode: vNode, path: [], distance: 0)];
+  List<DJNode> graphWithoutVNode =
+      graph.takeWhile((value) => value != vNode).toList();
+  List<Path> uList = graphWithoutVNode.map((node) {
+    var distance = 999;
+    List<DJNode> path = [];
+
+    List<Neighbor> vNeighbor =
+        node.neighbors.takeWhile((neighbor) => neighbor.node == vNode).toList();
+    if (vNeighbor.length != 0) {
+      distance = vNeighbor[0].weight;
+      path.add(node);
+    }
+    return Path(targetNode: node, path: path, distance: distance);
+  }).toList();
+
+  while (uList.length > 0) {
+    uList.sort((a, b) => a.distance - b.distance);
+    var u = uList.removeAt(0);
+    var uNode = u.targetNode;
+    sList.add(u);
+    uNode.neighbors.forEach((uNeighbor) {
+      uList.forEach((restUPath) {
+        if (restUPath.targetNode == uNeighbor.node &&
+            uNeighbor.weight + u.distance < restUPath.distance) {
+          restUPath.distance = uNeighbor.weight + u.distance;
+          // u.path.coypWithin <- what is this?
+          restUPath.path = u.path.map((e) => e).toList();
+          restUPath.path.add(restUPath.targetNode);
+        }
+      });
+    });
+  }
+
+  return sList;
 }

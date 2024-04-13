9407dfd170bf6bb9aa887db4aea6c95a9550db18
knightingal <yujianming1985@hotmail.com>
2024-04-13 21:38:56 +0800

    init dijkstra classes

--- lib/discover_china/script/computer_player.dart
+++ lib/discover_china/script/computer_player.dart
@@ -1,1 +1,25 @@
 class FindPath {}
+
+class DJNode {
+  final String nodeId;
+  List<Neighbor> neighbors = [];
+
+  DJNode({required this.nodeId});
+}
+
+class Neighbor {
+  final DJNode node;
+  final int weight;
+
+  Neighbor({required this.node, required this.weight});
+}
+
+class Path {
+  late DJNode targetNode;
+  List<DJNode> path = [];
+  int distance = 0;
+}
+
+List<Path> dijkstra(List<DJNode> graph, DJNode vNode) {
+  return [];
+}

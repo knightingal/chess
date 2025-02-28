ad5c27892e7a92ed92a94bdeb42f11ea5082b955
knightingal <yujianming1985@hotmail.com>
2025-02-28 20:50:54 +0800

    add 37 item

--- lib/demos/scroll.dart
+++ lib/demos/scroll.dart
@@ -30,7 +30,12 @@ class CustomScrollViewExample extends StatefulWidget {
 
 class _CustomScrollViewExampleState extends State<CustomScrollViewExample> {
   List<int> top = <int>[];
-  List<int> bottom = <int>[0, 1, 2, 3];
+  List<int> bottom = <int>[
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    30, 31, 32, 33, 34, 35, 36, 37,
+  ];
 
   @override
   Widget build(BuildContext context) {
